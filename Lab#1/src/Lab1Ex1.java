2c8641807c5e8f56b88025dcae249269095edf07
cmallen2106 <cmallen2106@gmail.com>
2021-07-08 10:52:03 -0600

    fixed typo in heading and redid exercise to get screenshots

--- Lab#1/src/Lab1Ex1.java
+++ Lab#1/src/Lab1Ex1.java
@@ -1,11 +1,11 @@
-//Lab 1 - Exercise 2
+//Lab 1 - Exercise 1
 //Christina Sosa
 //Programming Fundamentals, Summer 2021
 
 public class Lab1Ex1{
 
 	public static void main(String[] args) {
-		System.out.println("Hello World"); 
+		System.out.println("Hello World");
 	}
 }
 
