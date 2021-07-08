51a833c66fe54713b2a3ede7dd90a1528afdcfd2
cmallen2106 <cmallen2106@gmail.com>
2021-07-08 13:32:29 -0600

    Added testScore2

--- Lab1Ex2/src/Lab1Ex2/GradeAvg.java
+++ Lab1Ex2/src/Lab1Ex2/GradeAvg.java
@@ -18,10 +18,13 @@ public class GradeAvg {
 		double testScore1 = 78; 
 		System.out.println("Test Score 1 is " + testScore1 + "%"); 
 		
+		double testScore2 = 88; 
+		System.out.println("Test Score 2 is " + testScore2 + "%")
+		
 		double quizScoreAvg = ((quizScore1+quizScore2+quizScore3)/3); 
 		System.out.println("Quiz Score Average is " + quizScoreAvg + "%"); 
 		
-		double testScoreAvg = testScore1; 
+		double testScoreAvg = ((testScore1+testScore2)/2); 
 		System.out.println("Test Score Average is " + testScoreAvg + "%"); 
 		
 
