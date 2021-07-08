0364836ae48d7a3232f93855b97e0f841f83bab8
cmallen2106 <cmallen2106@gmail.com>
2021-07-08 09:29:31 -0600

    Lab 1 - Exercise 3 - Read back name and age

--- Lab1Ex3/src/AgeGuess.java
+++ Lab1Ex3/src/AgeGuess.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class AgeGuess {
 	public static void main(String[]args) {
+		Scanner scan = new Scanner (System.in);
+		
+		String name; 
+		int ageGuess; 
+		
+		System.out.println("What is your name?"); //Asking user for name
+		name = scan.nextLine(); 
+		
+		System.out.println("What is your age?"); //Asking user for age
+		ageGuess = scan.nextInt(); 
+		
+		//Reading out name and age to user
+		System.out.println("Hello, your name is " + name + " and your are " + ageGuess + " year's old.");
 		
 	}
 
